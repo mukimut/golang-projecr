83de9aba27b579ee0bdd8540b45c85eb64ea324d
saminnnn <saminnnn@gmail.com>
2021-02-06 00:09:41 +0600

    stream message complete

--- main.go
+++ main.go
@@ -4,123 +4,63 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strings"
+	"strconv"
+	"sync"
+	"time"
+
+	"github.com/gocarina/gocsv"
 )
 
-type ngninxBlock struct {
-	Start        int
-	End          int
-	AllLines     *[]*string
-	NestedBlocks []*ngninxBlock
-	totalBlocks  int
+type CliStreamerRecord struct {
+	Title       string `csv:"Title"`
+	Message1    string `csv:"Message 1"`
+	Message2    string `csv:"Message 2"`
+	StreamDelay int    `csv:"Stream Delay"`
+	RunTimes    int    `csv:"Run Times"`
 }
 
-type ngnixBlocks struct {
-	blocks   *[]*ngninxBlock
-	allLines *[]*string
-}
+var wg = sync.WaitGroup{}
+var ch = make(chan string, 50)
 
 func main() {
-	file, _ := os.Open("ngnix.conf")
-	scanner := bufio.NewScanner(file)
-	var lines []*string
-	var blockList []*ngninxBlock
+	wg.Add(2)
+	args := os.Args[1]
+	go processArgs(args)
+	messages := ""
+	file, _ := os.Create("./messages.txt")
+	writer := bufio.NewWriter(file)
 
-	for scanner.Scan() {
-		currentLine := scanner.Text()
-		lines = append(lines, &currentLine)
-	}
-	totalLines := len(lines)
-
-	for i := 0; i < totalLines; {
-		if strings.Contains(*lines[i], "{") {
-			currentBlock := getAllBlocks(lines, i, totalLines, 0, 1)
-			blockList = append(blockList, currentBlock)
-			i = currentBlock.End - 1
-			continue
+	go func(ch <-chan string) {
+		for message := range ch {
+			fmt.Println(message)
+			messages += message + "\n"
 		}
+		wg.Done()
+		writer.WriteString(messages)
+		writer.Flush()
+	}(ch)
 
-		i++
-	}
+	wg.Wait()
 
-	searchResult := getNgnixBlocks(blockList, "80")
-
-	for _, line := range *searchResult.allLines {
-		var lineString string = *line
-		fmt.Println(lineString)
-	}
 }
 
-func getNgnixBlock(lines *[]*string, startIndex, endIndex, recursionMax int) *ngninxBlock {
-	return getAllBlocks(*lines, startIndex, endIndex, 0, recursionMax)
-}
+func processArgs(arg string) {
+	var cliStreamers []CliStreamerRecord
+	gocsv.UnmarshalString(arg, &cliStreamers)
+	fmt.Println(cliStreamers)
 
-func getAllBlocks(lines []*string, start int, lineCount int, currentRecursion, maxRecursion int) *ngninxBlock {
-	var blockLines []*string
-	var nestedBlocks []*ngninxBlock
-	var end int
-	totalBlocks := 0
+	for i := 0; i < cliStreamers[0].RunTimes; i++ {
+		message := "CLI Invoke " + strconv.Itoa(i+1) + " " + cliStreamers[0].Message1
+		fmt.Println(message)
+		ch <- message
+		time.Sleep(time.Duration(cliStreamers[0].StreamDelay * int(time.Second)))
 
-	blockLines = append(blockLines, lines[start])
-	start++
-
-	for i := start; i < lineCount; {
-		blockLines = append(blockLines, lines[i])
-		if strings.Contains(*lines[i], "}") {
-			end = i + 1
-			break
-		}
-
-		if strings.Contains(*lines[i], "{") {
-			newBlock := getAllBlocks(lines, i, lineCount, currentRecursion+1, maxRecursion)
-			i = newBlock.End
-
-			if maxRecursion == -1 || currentRecursion <= maxRecursion {
-				nestedBlocks = append(nestedBlocks, newBlock)
-				totalBlocks++
-			}
-			continue
-		}
-
-		i++
+		message = "CLI Invoke " + strconv.Itoa(i+1) + " " + cliStreamers[0].Message2
+		fmt.Println(message)
+		ch <- message
+		time.Sleep(time.Duration(cliStreamers[0].StreamDelay * int(time.Second)))
 	}
 
-	// if maxRecursion == -1 || currentRecursion <= maxRecursion {
-	// 	testPrinter(*lines[start-1], start)
-	// 	testPrinter(*lines[end-1], end)
-	// 	fmt.Println(currentRecursion)
-	// 	fmt.Println(maxRecursion)
-	// }
-
-	return &ngninxBlock{start, end, &blockLines, nestedBlocks, totalBlocks}
-}
-
-func getNgnixBlocks(allBlocks []*ngninxBlock, config string) *ngnixBlocks {
-	var foundBlocks []*ngninxBlock
-	var foundLines []*string
-
-	for _, block := range allBlocks {
-		if block.totalBlocks > 0 {
-			nestedSearchResult := getNgnixBlocks(block.NestedBlocks, config)
-			foundBlocks = append(foundBlocks, *nestedSearchResult.blocks...)
-			foundLines = append(foundLines, *nestedSearchResult.allLines...)
-		}
-
-		for _, line := range *block.AllLines {
-			if strings.Contains(*line, config) {
-				foundBlocks = append(foundBlocks, block)
-				foundLines = append(foundLines, *block.AllLines...)
-				break
-			}
-		}
-	}
-
-	return &ngnixBlocks{&foundBlocks, &foundLines}
-}
-
-func testPrinter(line string, value int) string {
-	fmt.Print("Text is " + line + "|| ")
-	fmt.Print("Value is: ")
-	fmt.Println(value)
-	return line
+	close(ch)
+	wg.Done()
 }
