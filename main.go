2ea7fceea1146fcd154513853613db612b461901
saminnnn <saminnnn@gmail.com>
2021-02-06 21:37:52 +0600

    Integration test

--- main.go
+++ main.go
@@ -3,64 +3,93 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"os"
+	"os/exec"
 	"strconv"
-	"sync"
 	"time"
-
-	"github.com/gocarina/gocsv"
 )
 
-type CliStreamerRecord struct {
-	Title       string `csv:"Title"`
-	Message1    string `csv:"Message 1"`
-	Message2    string `csv:"Message 2"`
-	StreamDelay int    `csv:"Stream Delay"`
-	RunTimes    int    `csv:"Run Times"`
-}
-
-var wg = sync.WaitGroup{}
-var ch = make(chan string, 50)
-
 func main() {
-	wg.Add(2)
-	args := os.Args[1]
-	go processArgs(args)
-	messages := ""
-	file, _ := os.Create("./messages.txt")
-	writer := bufio.NewWriter(file)
+	message1 := "hello1"
+	message2 := "hola2"
+	delay := 1
+	repeat := 3
+	command := "Title,Message 1,Message 2,Stream Delay,Run Times \nCLI Invoker Name," + message1 + "," + message2 + "," + strconv.Itoa(delay) + "," + strconv.Itoa(repeat) + " "
 
-	go func(ch <-chan string) {
-		for message := range ch {
-			fmt.Println(message)
-			messages += message + "\n"
-		}
-		wg.Done()
-		writer.WriteString(messages)
-		writer.Flush()
-	}(ch)
+	readCommand(command, repeat, message1, message2)
 
-	wg.Wait()
+	time.Sleep(100 * time.Millisecond)
 
 }
 
-func processArgs(arg string) {
-	var cliStreamers []CliStreamerRecord
-	gocsv.UnmarshalString(arg, &cliStreamers)
-	fmt.Println(cliStreamers)
+func readCommand(args string, repeat int, message1, message2 string) {
+	cmd := exec.Command("./golang-project", args)
+	cmdReader, err := cmd.StdoutPipe()
+	var messages []string
 
-	for i := 0; i < cliStreamers[0].RunTimes; i++ {
-		message := "CLI Invoke " + strconv.Itoa(i+1) + " " + cliStreamers[0].Message1
-		fmt.Println(message)
-		ch <- message
-		time.Sleep(time.Duration(cliStreamers[0].StreamDelay * int(time.Second)))
+	if err != nil {
+		fmt.Println(err)
+	}
+	scanner := bufio.NewScanner(cmdReader)
 
-		message = "CLI Invoke " + strconv.Itoa(i+1) + " " + cliStreamers[0].Message2
-		fmt.Println(message)
-		ch <- message
-		time.Sleep(time.Duration(cliStreamers[0].StreamDelay * int(time.Second)))
+	go func() {
+		for scanner.Scan() {
+			messages = append(messages, scanner.Text())
+		}
+		test(messages, message1, message2, repeat)
+	}()
+
+	if err := cmd.Start(); err != nil {
+		fmt.Println(err)
+	}
+	if err := cmd.Wait(); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func test(allMessages []string, message1 string, message2 string, repeats int) {
+	pass := true
+	for i := 1; i <= repeats; i++ {
+		currentPass := false
+
+		for _, line := range allMessages {
+			if checkMessage(line, message1, i) {
+				currentPass = true
+				break
+			}
+		}
+
+		if !currentPass {
+			pass = false
+			fmt.Println("Faile at iteration " + strconv.Itoa(i) + " of " + message1)
+		}
+		currentPass = false
+
+		for _, line := range allMessages {
+			if checkMessage(line, message2, i) {
+				currentPass = true
+				break
+			}
+		}
+
+		if !currentPass {
+			pass = false
+			fmt.Println("Faile at iteration " + strconv.Itoa(i) + " of " + message1)
+		}
+
+		if !pass {
+			break
+		}
+
+		if pass {
+			fmt.Print("PASSED")
+		} else {
+			fmt.Print("FAILED")
+		}
+
 	}
 
-	close(ch)
-	wg.Done()
+}
+
+func checkMessage(line, testMessage string, iteration int) bool {
+	return line == "CLI Invoke "+strconv.Itoa(iteration)+" "+testMessage
 }
